20cb9f1fb68577d250c8ccbfb2e649b03ca0e0fc
ahmedroa <aahmedkro@gmail.com>
2023-07-15 01:22:01 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/gemaxo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gemaxo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/my_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color white = Color(0xfffefefe);
+  static const Color palyX = Color(0xfffd9f3e);
+  static const Color palyy = Color(0xff008eaa);
+  static const Color blue = Colors.blue;
+  static const Color truee = Color(0xff005387);
+}
